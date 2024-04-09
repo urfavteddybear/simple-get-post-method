220beb2de70c34b66ad0cb8393d10aa24608aade
unknown <business.urfavteddybear@gmail.com>
2024-04-10 00:12:59 +0800

    feat: Initial commit

---
+++ GET/demo_request.php
@@ -0,0 +1,8 @@
+<?php
+    $name = $_POST['fname'];
+    $nim = $_POST['fnim'];
+    $kelas = $_POST['fkelas'];
+    echo "NAMA : $name <br>";
+    echo "NIM : $nim <br>";
+    echo "KELAS : $kelas <br>";
+?>
